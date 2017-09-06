09b73984554248d87d3399d394ecea9ecc2b126d
monkut <5i!v3r-gh>
2017-09-07 03:19:00 +0900

    adding support for 'next' in column cards retrieve!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ghorgs',
-    version='0.1.4',
+    version='0.1.5',
     packages=['ghorgs'],
     url='https://github.com/monkut/github-org-manager',
     install_requires=['requests'],
